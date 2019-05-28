d37328c763e279c2d34849db635d8b85f4bf0df7
Abagail Hudson <abagailanne@gmail.com>
2019-05-28 00:10:33 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,15 @@
 class Post
- attr_accessor :author, :title
- @@all = []
- def initialize(title)
-   @title = title
-end
+  attr_accessor :author, :title 
+
+   def initialize(title)
+    @title = title
+  end
+
+   def author_name
+    if author 
+      self.author.name 
+    else
+      nil
+    end
+  end
 end
